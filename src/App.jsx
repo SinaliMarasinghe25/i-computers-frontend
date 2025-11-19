9a55baa9b8e437d2f1f5917b22d9cfa40ed89220
SinaliMarasinghe25 <sinalimarasingheuni01@gmail.com>
2025-11-19 10:51:02 +0530

    Day 11✏️

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,13 @@ function App() {
   
 
   return (
-    <div className= "w-24 h-24 border-amber-500 bg-emerald-300">
+    
+    <div className= "w-[500px] h-[500px] border-amber-500 bg-emerald-300 flex justify-center "  >
+      <div className="w-[400px] h-[400px] border-b-cyan-500 bg-amber-700 relative flex flex-col items-center">
+        <div className="w-[100px] h-[100px] bg-[red] flex justify-center  "></div>
+        <div className="w-[100px] h-[100px] bg-[yellow]"></div>
+        <div className="w-[100px] h-[100px] bg-[purple] flex items-center absolute left-[50px] top-[50px]"></div>
+      </div>
 
     </div>
   )
